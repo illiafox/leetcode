913ef5e95c836ee763aac9297aafae8dc78ec778
Illia Dymura <illiadimura@gmail.com>
2025-04-07 23:27:59 +0200

    feat: easy:  guess_number_higher_or_lower.rs

---
+++ src/easy/guess_number_higher_or_lower.rs
@@ -0,0 +1,24 @@
+struct Solution;
+
+fn guess(num: i32) -> i32 {
+    panic!("implement me")
+}
+
+impl Solution {
+    unsafe fn guess_number(n: i32) -> i32 {
+        let mut a = 1;
+        let mut b = n + 1;
+        let mut mid = n;
+
+        loop {
+            match guess(mid) {
+                -1 => b = mid,
+                1 => a = mid,
+                0 => return mid,
+                _ => panic!("what"),
+            }
+
+            mid = ((a as i64 + b as i64) / 2) as i32;
+        }
+    }
+}
