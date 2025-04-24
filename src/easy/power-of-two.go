0d61c93d56ffe1a34eeeaa246f35a8ee8e01e2b1
Illia Dymura <illiadimura@gmail.com>
2025-04-25 00:14:14 +0200

    feat: easy: power-of-two.go: new solution

--- src/easy/power-of-two.go
+++ src/easy/power-of-two.go
@@ -1,6 +1,14 @@
 package easy
 
 func isPowerOfTwo(n int) bool {
+	if n == 0 {
+		return false
+	}
+
+	return (n-1)&n == 0
+}
+
+func isPowerOfTwoAlternative(n int) bool {
 	for n > 0 {
 		if n&1 == 1 {
 			return n == 1
