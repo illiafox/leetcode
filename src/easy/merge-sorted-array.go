59e51c02362c509ba379e70ca18d897fcd1396a2
Illia Dymura <illiadimura@gmail.com>
2025-05-31 00:58:04 +0200

    feat: easy: merge-sorted-array.go

---
+++ src/easy/merge-sorted-array.go
@@ -0,0 +1,21 @@
+package easy
+
+import "slices"
+
+// https://leetcode.com/problems/merge-sorted-array/
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	for n > 0 {
+		if m > 0 && nums1[m-1] > nums2[n-1] {
+			nums1[m+n-1] = nums1[m-1]
+			m--
+		} else {
+			nums1[m+n-1] = nums2[n-1]
+			n--
+		}
+	}
+}
+
+// naive solution
+func mergeNaive(nums1 []int, m int, nums2 []int, n int) {
+	slices.Sort(append(nums1[:m], nums2...))
+}
