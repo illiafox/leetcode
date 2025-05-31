751876416a05b34a2b9f176846470e56a48c6547
Illia Dymura <illiadimura@gmail.com>
2025-06-01 00:03:45 +0200

    feat: easy: valid-perfect-square.go: add todo

--- src/easy/valid-perfect-square.go
+++ src/easy/valid-perfect-square.go
@@ -2,6 +2,8 @@ package easy
 
 import "math"
 
+// https://leetcode.com/problems/valid-perfect-square
+// TODO: use binary search?
 func isPerfectSquare(num int) bool {
 	s := math.Sqrt(float64(num))
 	return s == math.Trunc(s)
