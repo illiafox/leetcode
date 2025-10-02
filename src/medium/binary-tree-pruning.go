ac49eb28df84828dd19f85185e679dcce5fe92b0
Illia Dymura <illiadimura@gmail.com>
2025-10-02 23:31:31 +0200

    feat(solutions): medium: binary-tree-pruning.go

---
+++ src/medium/binary-tree-pruning.go
@@ -0,0 +1,26 @@
+package medium
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+// https://leetcode.com/problems/binary-tree-pruning/
+func pruneTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	// post order
+	root.Left = pruneTree(root.Left)
+	root.Right = pruneTree(root.Right)
+
+	if root.Val == 0 && root.Left == nil && root.Right == nil {
+		return nil
+	}
+	return root
+}
